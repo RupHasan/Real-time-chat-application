be37af966a9e1360552a8b8ed624f6dd963214c9
Rup Hasan <hasanrup7@gmail.com>
2026-07-24 00:01:01 +0600

    Rough commit, working on socket io

---
+++ public/script.js
@@ -0,0 +1,19 @@
+// Autotype aka Marketing
+const autotypeSpeed = 200;
+
+let type = new Typed("#autoType", {
+    strings: [
+        "Programmer",
+        "Student",
+        "Web Developer",
+        "Front-end Developer",
+        "Back-end Developer",
+        "Full-stack Developer"
+    ],
+    typeSpeed: autotypeSpeed,
+    backSpeed: autotypeSpeed,
+    loop: true
+});
+
+
+function sendMsg() {}
