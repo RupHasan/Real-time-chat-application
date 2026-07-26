c4d376330f80fcf6c158769cbf18caac26f93808
Rup Hasan <hasanrup7@gmail.com>
2026-07-26 23:04:24 +0600

    New user adding feature created, needs a lot of polish

--- server.js
+++ server.js
@@ -12,6 +12,31 @@ const server = http.createServer(app);
 const io = socketIo(server);
 const port = process.env.PORT || 3000;
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+function userAuth() {}
+
+app.post("/auth", async (req, res) => {
+    try {
+        const { username, password, email, phnNumber } = req.body;
+        console.log(username, password, email, phnNumber);
+
+        const conn = await pool.getConnection();
+        const [results] = await conn.query(
+            "INSERT INTO chatappUsers (username, password, email, phnNumber) VALUES (?,?,?,?);",
+            [username, password, email, phnNumber]
+        );
+
+        conn.release();
+        res.send(results);
+    } catch (err) {
+        if (err.code == "ER_DUP_ENTRY") {
+            res.send("username already exists");
+        } else {
+            res.send("come on, a new err", err);
+        }
+    }
+});
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "dashboard.html"));
@@ -30,8 +55,8 @@ io.on("connection", async socket => {
             ["Anonymous", data]
         );
         conn.release();
-        
-        sendData = [{username: "Anonymous", msg: data}]
+
+        sendData = [{ username: "Anonymous", msg: data }];
         io.emit("getMsg", sendData);
     });
 });
