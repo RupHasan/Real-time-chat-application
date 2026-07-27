6d7dc91cb8752c3f285f41dc0b63a679ccc62647
Rup Hasan <hasanrup7@gmail.com>
2026-07-27 16:58:05 +0600

    Added userAuth middlewere and cookie things

--- server.js
+++ server.js
@@ -12,37 +12,76 @@ const server = http.createServer(app);
 const io = socketIo(server);
 const port = process.env.PORT || 3000;
 app.use(express.static("public"));
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-function userAuth() {}
+function authUser(req, res, next) {
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.send(`
+            <script>
+                alert("User is not logged in!");
+                window.location.href = "/auth"
+            </script>
+        `);
+    } else {
+        try {
+            const decode = jwt.verify(token, process.env.JWT_PASS);
+            req.user = decode;
+            next();
+        } catch (err) {
+            return res.send(`
+                <script>
+                    alert("Token is not verified! Must log in again.");
+                    window.location.href = "/auth"
+                </script>
+            `);
+        }
+    }
+}
 
 app.post("/auth", async (req, res) => {
+    let conn;
     try {
         const { username, password, email, phnNumber } = req.body;
-        console.log(username, password, email, phnNumber);
-
-        const conn = await pool.getConnection();
-        const [results] = await conn.query(
+        conn = await pool.getConnection();
+        const [createAccount] = await conn.query(
             "INSERT INTO chatappUsers (username, password, email, phnNumber) VALUES (?,?,?,?);",
             [username, password, email, phnNumber]
         );
+        const [results] = await conn.query(
+            "SELECT * FROM chatappUsers WHERE username = ?",
+            [username]
+        );
 
-        conn.release();
-        res.send(results);
+        // Jwt processing
+        const token = jwt.sign(
+            { userId: results[0].id, username: results[0].username },
+            process.env.JWT_PASS
+        );
+
+        res.cookie("token", token, { httpOnly: true }).send(results);
     } catch (err) {
         if (err.code == "ER_DUP_ENTRY") {
             res.send("username already exists");
         } else {
-            res.send("come on, a new err", err);
+            res.send(err.code);
         }
+    } finally {
+        conn.release();
     }
 });
 
-app.get("/", (req, res) => {
+app.get("/auth", (req,res)=>{
+    res.sendFile(path.join(__dirname, "public", "auth.html"))
+})
+
+app.get("/", authUser, (req, res) => {
     res.sendFile(path.join(__dirname, "public", "dashboard.html"));
 });
 
-io.on("connection", async socket => {
+io.on("connection", async (socket) => {
     const conn = await pool.getConnection();
     const [results] = await conn.query("SELECT * FROM chatapp;");
     socket.emit("onConnect", results);
